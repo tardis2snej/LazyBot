a8eb665aedfff27f36086a81aef119a8ccec2645
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-16 22:40:08 +0200

    вялые попытки сделать вебхук через gunicorn

--- project/bot.py
+++ project/bot.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 import telebot
 import config
+import os
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.TOKEN)
+server = Flask(__name__)
+
+
+@server.route("/" + config.TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route('/', methods=["GET"])
+def index():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(config.APP_NAME, config.TOKEN))
+    return "Hello from Heroku!", 200
 
 
 @bot.message_handler(commands=['start'])
@@ -18,10 +34,9 @@ def send_text(message):
         bot.send_sticker(message.chat.id, 'CAADAgADMwIAArrAlQWc3UwCquHIDhYE')
 
 
-@bot.message_handler(content_types=['sticker'])
-def display(message):
-    print(message)
-
-
 if __name__ == '__main__':
     bot.infinity_polling()
+
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
