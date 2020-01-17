b12463381a5383cada1a46f954517e5d1911107a
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-17 23:17:33 +0200

    Update app.py
    предыдущее выдавало ошибку 309 Permanent Redirect

--- app.py
+++ app.py
@@ -27,12 +27,21 @@ def answer(message):
 
 
 app = Flask(__name__)
-@app.route("/bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A/", methods=['POST'])
+
+
+@app.route("/bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A", methods=['POST'])
 def get_message():
+    print("POST METHOD")
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return 200
+
+
+@app.route("/", methods=['GET'])
+def index_page():
+    print("GET METHOD")
     return "!", 200
 
 
 print("BEFORE APP.RUN")
-app.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 print("AFTER APP.RUN")
