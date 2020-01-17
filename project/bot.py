dde3e6632a7aa4200c90949eba8be4fa56bf1208
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-17 15:22:27 +0200

    Revert "код уже не мой но пусть хотя бы заработает"
    This reverts commit 1b519b771e267a3f17a6b84e4e536bd4bf1290af.

---
+++ project/bot.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import telebot
+import os
+import config
+import flask
+from time import sleep
+
+bot = telebot.TeleBot(config.TOKEN)
+# PORT = int(os.environ.get('PORT', '80'))
+# print("PORT IS", PORT)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет! Как насчет ленивого разговора?")
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if 'как дела' in message.text.lower():
+        bot.send_message(message.chat.id, "Все отлично!")
+    elif 'ты классный' in message.text.lower():
+        bot.send_sticker(message.chat.id, 'CAADAgADMwIAArrAlQWc3UwCquHIDhYE')
+    else:
+        bot.send_message(message.chat.id, "Отличная погодка, не правда ли?")
+
+
+# Проверим, есть ли переменная окружения Хероку
+if "HEROKU" in list(os.environ.keys()):
+    print("HEROKU STARTING FLASK")
+    app = flask.Flask(__name__)
+    print("FLASK RUNNING")
+    bot.remove_webhook()
+    sleep(2)
+    bot.set_webhook(url="https://{}.herokuapp.com/bot{}".format(config.APP_NAME, config.TOKEN))
+    sleep(2)
+    print("WEBHOOK SET")
+
+    @app.route("/bot{}".format(config.TOKEN), methods=['POST'])
+    def get_message():
+        print("LET'S DO UPDATE")
+        if flask.request.headers.get('content-type') == 'application/json':
+            sleep(2)
+            json_string = flask.request.get_data().encode('utf-8')
+            sleep(2)
+            update = telebot.types.Update.de_json(json_string)
+            sleep(2)
+            bot.process_new_messages([update.message])
+            sleep(2)
+            return ''
+        else:
+            flask.abort(403)
+
+
+    @app.route("/")
+    def webhook():
+        print("INDEX PAGE")
+        return ".", 200
+
+    # if __name__ == '__main__':
+    print("START SERVER RUN")
+        # server.run(host="0.0.0.0", port=PORT)
+    app.run(threaded=True)
+    print("END SERVER RUN")
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    print("LONG POOLING")
+    bot.polling(none_stop=True)
--- telebot/credentials.py
+++
@@ -1,3 +0,0 @@
-bot_token = 'bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A'
-bot_user_name = "lazy-bot007"
-URL = "https://lazy-bot007.herokuapp.com/"
