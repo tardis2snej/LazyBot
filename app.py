3fe472e5a98a2119d231a4fff81d85dd395db2da
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-17 22:18:15 +0200

    приличными словами здесь нечего сказать

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import telebot
+from flask import Flask, request
+import logging
 
 bot = telebot.TeleBot('986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A')
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
 
 @bot.message_handler(commands=['start'])
@@ -13,4 +18,11 @@ def answer(message):
     bot.send_message(message.chat.id, ':)')
 
 
-bot.polling()
+app = Flask(__name__)
+@app.route("/bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A/", methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+app.run()
