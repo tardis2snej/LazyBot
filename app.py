856e57728704162430bbb68fd672d31f89cb3dc4
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-26 00:51:32 +0200

    опция пулинга с машины разработчика

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import time
 import telebot
 from flask import Flask, request
 import logging
+if "HEROKU" in list(os.environ.keys()):
+    TOKEN = os.environ.get('TOKEN')
+else:
+    import config
+    TOKEN = config.TOKEN
 
-bot = telebot.TeleBot('986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A')
+bot = telebot.TeleBot(TOKEN)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
 bot.remove_webhook()
 time.sleep(2)
-bot.set_webhook(url="https://lazy-bot007.herokuapp.com/bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A")
-print("WEBHOOK SET")
 
 
 @bot.message_handler(commands=['start'])
@@ -25,22 +28,29 @@ def answer(message):
     bot.send_message(message.chat.id, ':)')
 
 
-app = Flask(__name__)
+if "HEROKU" in list(os.environ.keys()):
+    bot.set_webhook(url="https://lazy-bot007.herokuapp.com/bot" + TOKEN)
+    print("WEBHOOK SET")
+
+    app = Flask(__name__)
+
+    @app.route("/bot" + TOKEN, methods=['POST'])
+    def get_message():
+        print("POST METHOD")
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
 
 
-@app.route("/bot986852722:AAHBNwKBZj3Brq5uk9l346Fn570cOI6dY3A", methods=['POST'])
-def get_message():
-    print("POST METHOD")
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+    @app.route("/", methods=['GET'])
+    def index_page():
+        print("GET METHOD")
+        return "!", 200
 
 
-@app.route("/", methods=['GET'])
-def index_page():
-    print("GET METHOD")
-    return "!", 200
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
+else:
+    print("DEV RUN")
+    bot.polling()
 
 
-print("BEFORE APP.RUN")
-app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-print("AFTER APP.RUN")
