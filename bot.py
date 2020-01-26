b6dc8a9dd85ed1332db8e6e1083c9515cce363ae
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-01-26 19:33:44 +0200

    splitted bot into setup file and handlers

---
+++ bot.py
@@ -0,0 +1,24 @@
+import telebot
+import bot_setup
+
+bot = telebot.TeleBot(bot_setup.get_token())
+bot_setup.setup(bot)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+@bot.message_handler()
+def answer(message):
+    bot.send_message(message.chat.id, ':)')
+
+
+bot_setup.start_bot(bot)
+
+
+
+
+
+
---
+++ bot_setup.py
@@ -0,0 +1,47 @@
+import os
+import telebot
+import logging
+import time
+from flask import Flask, request
+
+
+def get_token():
+    if "HEROKU" in list(os.environ.keys()):
+        token = os.environ.get('token')
+    else:
+        import config
+        token = config.TOKEN
+    return token
+
+
+def setup(bot):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    bot.remove_webhook()
+    time.sleep(2)
+
+
+def start_bot(bot):
+    if "HEROKU" in list(os.environ.keys()):
+        bot.set_webhook(url="https://lazy-bot007.herokuapp.com/bot" + get_token())
+        print("WEBHOOK SET")
+
+        app = Flask(__name__)
+
+        @app.route("/bot" + get_token(), methods=['POST'])
+        def get_message():
+            print("POST METHOD")
+            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
+
+        @app.route("/", methods=['GET'])
+        def index_page():
+            print("GET METHOD")
+            return "!", 200
+
+        app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
+    else:
+        print("DEV RUN")
+        bot.polling()
